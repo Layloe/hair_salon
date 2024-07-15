8c66a052983a86dfff963205ddaa6c65618c6a86
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-07-14 17:52:51 -0700

    hair salon barebones

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>The Mane Hair Salon</h1>
+        <nav>
+          <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a> 
+        </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  )
+}
+
+export default App
