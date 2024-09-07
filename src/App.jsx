2b20edce0652656b4a6d154a7941a4d41f825f3c
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-09-07 08:48:33 -0700

    testing navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./pages/Navbar";
 import { Outlet } from "react-router-dom";
+import NavbarStyle from "./pages/Navbar";
 
 const App = () => {
   return (
     <div>
       <header>
-        <Navbar />
+        <NavbarStyle />
         <h1>The Mane Hair Salon </h1>
         {/* <nav>
           <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a> 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,15 +1,31 @@
+import {  Navbar, Container, Nav } from "react-bootstrap"
 import React from "react";
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+// import Container from 'react-bootstrap/Container'
+// import Nav from 'react-bootstrap/Nav'
+// import Navbar from 'react-bootstrap/Navbar'
 import { NavLink } from "react-router-dom";
 
 //styling
 
 const NavbarStyle = () => {
     return (
+//   <nav className="navbar">
+//   <ul className="nav_links">
+//   <nav className="navbar">
+//   <ul className="nav_links">
+//     <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a> 
+//     </ul>
+//   </nav>
+//     <li><a href="/">Home</a></li>
+//     <li><a href="/staff">Staff</a></li>
+//     <li><a href="/services">Services</a></li>
+//     <li><a href="/booking">Booking</a></li>
+//     <li><a href="/admin">Amin</a></li> 
+//    </ul>
+//    </nav> 
+
         <>
-        <Navbar bg='primary'data-bs-theme='dark'>
+        <Navbar sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
                 <Navbar.Brand href='#home'>Mane Salon</Navbar.Brand>
                 <Nav className="me-auto">
