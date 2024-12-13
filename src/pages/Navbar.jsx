ddd73b654410869332ee83c453544563e742d563
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-12-13 06:57:44 -0800

    added react-time-picker

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -9,35 +9,35 @@ import { NavLink } from "react-router-dom";
 
 const NavbarStyle = () => {
     return (
-//   <nav className="navbar">
-//   <ul className="nav_links">
-//   <nav className="navbar">
-//   <ul className="nav_links">
-//     <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a> 
-//     </ul>
-//   </nav>
-//     <li><a href="/">Home</a></li>
-//     <li><a href="/staff">Staff</a></li>
-//     <li><a href="/services">Services</a></li>
-//     <li><a href="/booking">Booking</a></li>
-//     <li><a href="/admin">Amin</a></li> 
-//    </ul>
-//    </nav> 
+  <nav className="navbar">
+  <ul className="nav_links">
+  <nav className="navbar">
+  <ul className="nav_links">
+    <a href="/">Home</a> | <a href="/staff">Staff</a> | <a href="/services">Services</a> | <a href="/booking">Booking</a> | <a href="/admin">Admin</a> 
+    </ul>
+  </nav>
+    <li><a href="/">Home</a></li>
+    <li><a href="/staff">Staff</a></li>
+    <li><a href="/services">Services</a></li>
+    <li><a href="/booking">Booking</a></li>
+    <li><a href="/admin">Amin</a></li> 
+   </ul>
+   </nav> 
 
-        <>
-        <Navbar sticky="top" className="bg-white shadow-sm mb-3">
-            <Container>
-                {/* <Navbar.Brand href='#home'>Mane Salon</Navbar.Brand> */}
-                <Nav className="me-auto">
-                <Nav.Link to="/" as ={NavLink}>Home</Nav.Link>
-                    <Nav.Link href='/staff'>Staff</Nav.Link>
-                    <Nav.Link href='/services'>Services</Nav.Link>
-                    <Nav.Link href='/booking'>Booking</Nav.Link>
-                    <Nav.Link href='/admin'>Admin</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-        </>
+        // <>
+        // <Navbar sticky="top" className="bg-white shadow-sm mb-3">
+        //     <Container>
+        //         {/* <Navbar.Brand href='#home'>Mane Salon</Navbar.Brand> */}
+        //         <Nav className="me-auto">
+        //         <Nav.Link to="/" as ={NavLink}>Home</Nav.Link>
+        //             <Nav.Link href='/staff'>Staff</Nav.Link>
+        //             <Nav.Link href='/services'>Services</Nav.Link>
+        //             <Nav.Link href='/booking'>Booking</Nav.Link>
+        //             <Nav.Link href='/admin'>Admin</Nav.Link>
+        //         </Nav>
+        //     </Container>
+        // </Navbar>
+        // </>
     )
 }
 
